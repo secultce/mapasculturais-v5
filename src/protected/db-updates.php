354784f35651719f9f1c12facb907e6906b01fd1
Junior-Shyko <franciscaoanto@gmail.com>
2024-04-01 15:32:02 -0300

    :wrench: Migração do banco
    retirado o db-update de dentro da pasta do modulo e exportado para a estrutura padrão

--- src/protected/db-updates.php
+++ src/protected/db-updates.php
@@ -1909,5 +1909,31 @@ $$
                 p1.object_type = p2.object_type AND 
                 p1.object_id = p2.object_id AND 
                 p1.action = p2.action;");
+    },
+    'create table diligence' => function(){
+        __exec("CREATE SEQUENCE diligence_id_seq INCREMENT BY 1 MINVALUE 1 START 1;");
+
+        __exec("CREATE TABLE IF NOT EXISTS diligence (
+            id INT NOT NULL, 
+            registration_id INT NOT NULL, 
+            open_agent_id INT NOT NULL, 
+            agent_id VARCHAR(32) NOT NULL,
+            create_timestamp timestamp,
+            description TEXT,
+            status INT NOT NULL,
+            situation INT NULL,
+            send_diligence timestamp,
+            PRIMARY KEY(id));");
+
+        __exec("ALTER TABLE diligence ADD
+        CONSTRAINT diligence_registration_fk
+        FOREIGN KEY (registration_id) REFERENCES registration (id)
+        ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE");
+
+        __exec("ALTER TABLE diligence ADD
+        CONSTRAINT diligence_open_agent_id_fk
+        FOREIGN KEY (open_agent_id) REFERENCES agent (id)
+        ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE");
+        // $conn->executeQuery("UPDATE evaluation_method_configuration SET type = 'documentary' WHERE opportunity_id IN ({$ids})");
     }
 ] + $updates ;
