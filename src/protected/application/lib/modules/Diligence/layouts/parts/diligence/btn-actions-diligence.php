4510959663c52cef5c2ff8605235565476f52b84
Junior-Shyko <franciscaoanto@gmail.com>
2024-04-09 17:10:58 -0300

    :lipstick: Formatando botoes
    Formatando botoes do avaliador

--- src/protected/application/lib/modules/Diligence/layouts/parts/diligence/btn-actions-diligence.php
+++ src/protected/application/lib/modules/Diligence/layouts/parts/diligence/btn-actions-diligence.php
@@ -2,7 +2,7 @@
     <i class="fas fa-check-circle mr-10"></i>
     Suas alterações foram salvas
 </label>
-<button class="btn-send-diligence mr-10" title="Salva o conteúdo mas não envia sua resposta" 
+<button class="btn-save-diligence mr-10" title="Salva o conteúdo mas não envia sua resposta" 
     id="btn-save-diligence" 
     onclick="saveDiligence(0)"
 >
@@ -13,6 +13,6 @@
     title="Salva e envia a sua resposta para a comissão avaliadora." 
     onclick="saveDiligence(3)"
 >
-    Enviar
+    Enviar Diligência
     <i class="fas fa-paper-plane"></i>
 </button>
