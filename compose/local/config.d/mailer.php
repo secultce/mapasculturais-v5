1e430e9eacf3cfaae945b2854e33ceaf6d7d4b99
Junior-Shyko <franciscoanto@gmail.com>
2024-05-17 19:04:05 -0300

    :construction: #63 - Add arquivos de conf para desenvolvimento
    Add importe dos eventos, templates de emails e logs

--- compose/local/config.d/mailer.php
+++ compose/local/config.d/mailer.php
@@ -1,11 +1,65 @@
 <?php
+use \MapasCulturais\i;
 
 return [
-    // 'mailer.user'       => '',
-    // 'mailer.psw'        => '',
-    // 'mailer.protocol'   => 'TLS',
-    // 'mailer.server'     => 'smtp.mailtrap.io',
-    // 'mailer.port'       => '587',
-    // 'mailer.from'       => 'mapas@local',
-    // 'mailer.alwaysTo'   => false,
+    'mailer.user'       => '',
+    'mailer.psw'        => '',
+    'mailer.protocol'   => '',
+    'mailer.server'     => 'mailhog',
+    'mailer.port'       => '1025',
+    'mailer.from'       => 'mapas@local',
+
+    // se quiser redirecionar o envio de todos os emails para um mesmo endereço
+    'mailer.alwaysTo'   => false,
+    'mailer.templates' => [
+        'welcome' => [
+            'title' => i::__("Bem-vindo(a) ao Mapas Culturais"),
+            'template' => 'welcome.html'
+        ],
+        'last_login' => [
+            'title' => i::__("Acesse a Mapas Culturais"),
+            'template' => 'last_login.html'
+        ],
+        'new' => [
+            'title' => i::__("Novo registro"),
+            'template' => 'new.html'
+        ],
+        'update_required' => [
+            'title' => i::__("Acesse a Mapas Culturais"),
+            'template' => 'update_required.html'
+        ],
+        'compliant' => [
+            'title' => i::__("Denúncia - Mapas Culturais"),
+            'template' => 'compliant.html'
+        ],
+        'suggestion' => [
+            'title' => i::__("Mensagem - Mapas Culturais"),
+            'template' => 'suggestion.html'
+        ],
+        'seal_toexpire' => [
+            'title' => i::__("Selo Certificador Expirando"),
+            'template' => 'seal_toexpire.html'
+        ],
+        'seal_expired' => [
+            'title' => i::__("Selo Certificador Expirado"),
+            'template' => 'seal_expired.html'
+        ],
+        'opportunity_claim' => [
+            'title' => i::__("Solicitação de Recurso de Oportunidade"),
+            'template' => 'opportunity_claim.html'
+        ],
+        'request_relation' => [
+            'title' => i::__("Solicitação de requisição"),
+            'template' => 'request_relation.html'
+        ],
+        'start_registration' => [
+            'title' => i::__("Inscrição iniciada"),
+            'template' => 'start_registration.html'
+        ],
+        'send_registration' => [
+            'title' => i::__("Inscrição enviada"),
+            'template' => 'send_registration.html'
+        ],
+
+    ]
 ];
