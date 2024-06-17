821373ffb7faedc7db54fb43133953ca17da2729
Junior-Shyko <franciscoanto@gmail.com>
2024-06-17 15:00:41 -0300

    :wrench: Add configuração para amb. local
    add arquivo de plugins.php

--- compose/local/config.d/plugins.php
+++ compose/local/config.d/plugins.php
@@ -1,1 +1,18 @@
 <?php
+
+
+return [
+    'plugins' => [
+        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
+        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
+        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
+        'EvaluationMethodQualification' => ['namespace' => 'EvaluationMethodQualification'],
+        'ValuersManagement' => ['namespace' => 'ValuersManagement'],
+        'OpinionManagement' => ['namespace' => 'OpinionManagement'],
+        'Recourse' => ['namespace' => 'Recourse'],
+        'EditOpportunityType' => ['namespace' => 'EditOpportunityType'],
+        'LocationStateCity' => ['namespace' => 'LocationStateCity'],
+        'MultipleLocalAuth' => ['namespace' => 'MultipleLocalAuth'],
+        'PDFReport' => ['namespace' => 'PDFReport']
+    ]
+];
