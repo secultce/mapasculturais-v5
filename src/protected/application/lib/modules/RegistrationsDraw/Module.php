9decec9f489061f966296dd51600a9f9bb60826f
Ronny John <me@ronnyjohnti.dev>
2024-06-18 16:51:33 -0300

    feat: #98 Preparando validações

--- src/protected/application/lib/modules/RegistrationsDraw/Module.php
+++ src/protected/application/lib/modules/RegistrationsDraw/Module.php
@@ -12,16 +12,37 @@ class Module extends \MapasCulturais\Module
         $app = App::i();
 
         $app->hook('template(opportunity.single.opportunity-support--tab):after', function () {
+            /**
+             * @var \MapasCulturais\Entities\Opportunity $opportunity
+             * @var bool $drawSetted
+             */
+            $opportunity = $this->controller->requestedEntity;
+            // todo: Implements draw setting
+            $drawSetted = true;
+
+            /** Skip the hook when opportunity not setted to prize draw or user can't control this opportunity */
+            if(!$drawSetted || !$opportunity->canUser('@control'))
+                return;
+
             $this->part('opportunity/tab-draw');
         });
 
         $app->hook('template(opportunity.single.tabs-content):end', function () use ($app) {
             /**
              * @var \MapasCulturais\Controllers\Opportunity $this
+             * @var \MapasCulturais\Entities\Opportunity $opportunity
+             * @var bool $drawSetted
              */
+            $opportunity = $this->controller->requestedEntity;
+            // todo: Implements draw setting
+            $drawSetted = true;
+
+            /** Skip the hook when the user can't control this opportunity */
+            if(!$drawSetted || !$opportunity->canUser('@control'))
+                return;
 
             $app->view->enqueueStyle('app', 'prize-draw', 'css/prize-draw.css');
-            $categories = $this->controller->requestedEntity->registrationCategories;
+            $categories = $opportunity->registrationCategories;
             $this->part('opportunity/prize-draw-content', ['categories' => $categories]);
         });
     }
