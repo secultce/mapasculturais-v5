df82f6dcb9e23a0c08d72b2af1ef2b6643b4012b
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-06-18 17:25:05 -0300

    Referência do plugin PublicConsultation

--- src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/accountability-nav-panel.php
+++ src/protected/application/lib/modules/OpportunityAccountability/layouts/parts/accountability/accountability-nav-panel.php
@@ -1,6 +1,8 @@
+<?php $this->applyTemplateHook('nav.panel.accountability', 'before'); ?>
 <li>
-    <a <?php if($this->template == 'panel/accountabilitys') echo 'class="active"'; ?> href="<?php echo $app->createUrl('panel', 'accountability') ?>">
-        <span class="icon icon-opportunity"></span> 
-        <?php \MapasCulturais\i::_e("Minhas prestações de contas");?>
+    <a <?php if ($this->template == 'panel/accountabilitys') echo 'class="active"'; ?> href="<?php echo $app->createUrl('panel', 'accountability') ?>">
+        <span class="icon icon-opportunity"></span>
+        <?php \MapasCulturais\i::_e("Minhas prestações de contas"); ?>
     </a>
 </li>
+<?php $this->applyTemplateHook('nav.panel.accountability', 'after'); ?>
