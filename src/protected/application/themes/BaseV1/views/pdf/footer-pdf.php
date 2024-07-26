b934be678ec2b2ccb59a35a50b8b33b8da4db998
Junior-Shyko <franciscaoanto@gmail.com>
2024-07-26 15:38:15 -0300

    :children_crossing: #165 - Add imagem no rodape da I.V Secult

--- src/protected/application/themes/BaseV1/views/pdf/footer-pdf.php
+++ src/protected/application/themes/BaseV1/views/pdf/footer-pdf.php
@@ -1,15 +1,19 @@
-<div style="width: 100%; float: left;">
-    <p style="text-align: center; padding-right: 30px; font-size: 10px;"><span> {PAGENO}{nbpg}</span></p>
-    <p style="width: 100%; float: left;font-size: 9px;">
-        <label for="">
-            <strong>Secretaria da Cultura do Ceara</strong>
-        </label><br>
-        <label for="">Rua Major Facundo, 500 - Centro. CEP: 60.025-100</label><br>
-        <label for="">Fortaleza/CE. Fone: (85) 31016767</label>
-    </p>   
-</div>
-<div style="width: 100%; text-align: center; position: absolute; bottom: 0;">
- 
-<img src="<?php echo THEMES_PATH . 'BaseV1/assets/img/rodape_timbrado.png'; ?>" 
-style="width: 100%; height: auto;" alt="">
-</div>
+</main>
+<footer style="position: relative; width: 110%">
+    <div style="width: 100%; float: left; margin: 25px">
+        <p style="text-align: center; padding-right: 30px; font-size: 10px;"><span> {PAGENO}{nbpg}</span></p>
+        <p style="width: 100%; float: left;font-size: 9px;">
+            <label for="">
+                <strong>Secretaria da Cultura do Ceara</strong>
+            </label><br>
+            <label for="">Rua Major Facundo, 500 - Centro. CEP: 60.025-100</label><br>
+            <label for="">Fortaleza/CE. Fone: (85) 31016767</label>
+        </p>
+    </div>
+    <div style="width: 100%; text-align: center; position: absolute; left: -10px;">
+        <img src="<?php echo THEMES_PATH . 'BaseV1/assets/img/rodape_timbrado.png'; ?>" style="width: 100%; height: auto;" alt="">
+    </div>
+</footer>
+
+    </body>
+</html>
