e52cb2d8e2f466997a773cc9701935f76a0870a1
Junior-Shyko <franciscaoanto@gmail.com>
2024-07-31 10:47:56 -0300

    :coffin: #173 - Removendo codigos desnecessários

--- src/protected/application/lib/modules/Diligence/layouts/parts/multi/btn-generate-tado.php
+++ src/protected/application/lib/modules/Diligence/layouts/parts/multi/btn-generate-tado.php
@@ -13,19 +13,6 @@ foreach ($reg->opportunity->getMetadata() as $key => $value) {
     </p>
 </div>
 <?php
-if($enableBtn) {
-?>
-<p style="text-align: center">
-    <!-- <a href="<?= $app->createUrl('tado', 'emitir/' . $reg->id); ?>"
-        class="btn btn-primary"
-        title="Gera o relatório TADO"
-    >
-        Emitir TADO
-    </a> -->
-</p>
-<?php
-  }
-
     $this->part('multi/multi-select', ['reg' => $reg]);
 ?>
 
