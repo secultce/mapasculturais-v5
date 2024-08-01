6b4349b5961768747f3d34f37691d93096464244
Ronny John <me@ronnyjohnti.dev>
2024-08-01 16:49:31 -0300

    chore: Adicionando plugin BigSheetImporter

--- compose/local/config.d/plugins.php
+++ compose/local/config.d/plugins.php
@@ -14,5 +14,6 @@ return [
         'MultipleLocalAuth' => ['namespace' => 'MultipleLocalAuth'],
         'PublicConsultation' => ['namespace' => 'PublicConsultation'],
         'PDFReport' => ['namespace' => 'PDFReport'],
+        'BigSheetImporter' => ['namespace' => 'BigSheetImporter'],
     ]
 ];
