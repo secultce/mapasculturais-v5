9179a0c709915b4daaca5458e93be7fc7919ef84
Ronny John <me@ronnyjohnti.dev>
2024-09-04 14:56:53 -0300

    feat: Adicionando validação de acesso do usuário

--- src/protected/application/lib/modules/QuotasSet/Module.php
+++ src/protected/application/lib/modules/QuotasSet/Module.php
@@ -8,25 +8,45 @@ use MapasCulturais\Theme;
 
 class Module extends \MapasCulturais\Module
 {
+    /** @var App $app */
+    private $app;
 
     public function _init(): void
     {
-        $app = App::getInstance();
+        $this->app = App::getInstance();
+        $module = $this;
 
-        $app->hook('template(panel.<<*>>.nav.panel.accountability):after', function () {
+        $this->app->hook('template(panel.<<*>>.nav.panel.accountability):after', function () use ($module) {
             /** @var Theme $this */
-            $this->part('quotas-set.nav.panel');
+            if ($module->canUserAccess()) {
+                $this->part('quotas-set.nav.panel');
+            }
         });
 
-        $app->hook('GET(panel.cotas-e-politicas)', function () {
+        $this->app->hook('GET(panel.cotas-e-politicas)', function () use ($module) {
             /** @var Controller $this */
             $this->requireAuthentication();
-            $this->render('panel-quotas-set');
+            if ($module->canUserAccess()) {
+                $this->render('panel-quotas-set');
+            } else {
+                $module->app->redirect('/panel');
+            }
         });
     }
 
     public function register(): void
     {
         // TODO: Implement register() method.
+    }
+
+    private function canUserAccess(): bool
+    {
+        foreach ($this->app->user->profile->sealRelations as $sealRelation) {
+            if ($sealRelation->seal->id == env('SECULT_SEAL_ID')) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
