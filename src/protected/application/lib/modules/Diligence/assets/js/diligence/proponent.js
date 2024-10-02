8586de062021324ddb0706d5a1ff7af3a4d86e97
Ronny John <me@ronnyjohnti.dev>
2024-10-02 11:12:09 -0300

    fix: #220 Corrigindo campo de resposta sendo mostrado mesmo quando não tinha diligência

--- src/protected/application/lib/modules/Diligence/assets/js/diligence/proponent.js
+++ src/protected/application/lib/modules/Diligence/assets/js/diligence/proponent.js
@@ -18,32 +18,26 @@ $(document).ready(function () {
     let entityDiligence = EntityDiligence.showContentDiligence();
     entityDiligence
     .then((res) => {
+        console.log(res)
         const draftStatus = 0;
         const diligences = res.data;
-        const diligenceSent = diligences?.filter( diligence => {
-            return diligence?.status != draftStatus;
+        const diligenceSent = diligences?.filter(diligence => {
+            return diligence?.status > draftStatus;
         });
        
-        if (
-            (res.message == 'sem_diligencia') &&
-            MapasCulturais.isEvaluator == false)
-        {
+        if (res.message === 'sem_diligencia' && MapasCulturais.isEvaluator === false) {
             //Se tiver diligencia
             if (res.data?.length && diligenceSent.length) {
                 const ahref ='<a href="#diligence-diligence" rel="noopener noreferrer" onclick="hideRegistration()" id="tab-main-content-diligence-diligence">Diligência</a>';
-                
+
                 $("#li-tab-diligence-diligence > label").removeClass('cursor-disabled');
                 $("#li-tab-diligence-diligence > label").remove();
                 $("#li-tab-diligence-diligence").append(ahref);
 
                 res.data.forEach((element, index) => {
-                    const dateLimitDate = EntityDiligence.validateLimiteDate(MapasCulturais.diligence_days);
-                    if (dateLimitDate) {
-                        $("#descriptionDiligence").hide();
-                        $("#div-btn-actions-proponent").hide();
-                    }else{
-                        $("#div-btn-actions-proponent").show();
-                    }
+                    $("#descriptionDiligence").hide();
+                    $("#div-btn-actions-proponent").hide();
+                    $("#attachment-info").hide();
                     //Id da diligencia
                     MapasCulturais.idDiligence = element?.id;
                     $("#paragraph_loading_content").hide();
@@ -53,16 +47,15 @@ $(document).ready(function () {
 
         if(res.message !== 'sem_diligencia' &&  MapasCulturais.isEvaluator === false) {
             hideAnswerDraft();
-            idsDiligences = [];
+            let idsDiligences = [];
             res.data.forEach((answer, index) => {
-                if(answer?.id === undefined)
-                {
+                if (answer?.id === undefined) {
                     EntityDiligence.showAnswerDraft(null);
                     $("#descriptionDiligence").show();
-                }else{
+                } else {
                     idsDiligences.push(answer?.id);
                 }
-                if(answer?.status == 0) {
+                if (answer?.status === 0) {
                     $("#descriptionDiligence").hide();
                 }
             })
@@ -75,11 +68,11 @@ $(document).ready(function () {
             res.data.forEach((answer, index) => {
                 const limitDate = EntityDiligence.validateLimiteDate(MapasCulturais.diligence_days);
 
-                if(limitDate){
+                if (limitDate) {
                     EntityDiligence.showAnswerDraft(answer);
                     $("#descriptionDiligence").hide();
                     $("#div-btn-actions-proponent").hide();
-                }else{
+                } else {
                     MapasCulturais.idDiligence = answer?.diligence?.id;
                     EntityDiligence.showAnswerDraft(answer);
                     $("#descriptionDiligence").show();
