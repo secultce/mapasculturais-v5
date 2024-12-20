066334007eb0f2f8b8dc3c1f73344d8392f2b2e6
Junior-Shyko <franciscoanto@gmail.com>
2024-12-20 10:49:41 -0300

    ♻️ #337 - Refatorando classe de notificacao

--- src/protected/application/lib/modules/Diligence/Entities/NotificationDiligence.php
+++ src/protected/application/lib/modules/Diligence/Entities/NotificationDiligence.php
@@ -9,26 +9,18 @@ class NotificationDiligence {
 
     public function create($class, $type)
     {
-       
         $app = App::i();
         $notification = new Notification();
-        $userNotifi = null;
+        // Mensagem para notificação na plataforma
         if($type == 'diligence') {
             $agent = $app->repo('Agent')->find($class->data['agent']);
-            $url = $app->createUrl('inscricao', $class->data['registration']);
-            //Mensagem para notificação na plataforma
-            $numberRegis = '<a href="'.$url.'">'.$class->data['registration'].'</a>';
-            $msgSend = 'Um parecerista abriu uma diligência para você responder na inscrição de número: '.$numberRegis;
-            $userNotifi = $agent->user;
+            $notification->message = $this->generateMessage($class, $type);
+            $notification->user = $agent->user;
         }else{
             $dili = $app->repo('Diligence\Entities\Diligence')->findOneBy(['registration' => $class->data['registration']]);
-            $numberRegis = '<a href="'.$app->createUrl('inscricao', $class->data['registration']).'">'.$class->data['registration'].'</a>';
-            $msgSend = "Houve uma resposta para prestação de conta de número: ".$numberRegis;
-            $userNotifi = $dili->openAgent->user;
+            $notification->message = $this->generateMessage($class, $type);
+            $notification->user = $dili->openAgent->user;
         }
-        $notification->message  = $msgSend;
-        $notification->user     = $userNotifi;
-        
         $app->disableAccessControl();
         $notification->save(true);
         $app->enableAccessControl();
@@ -41,7 +33,7 @@ class NotificationDiligence {
             $class->data['openAgent'],
             $class->data['agent']
         );
-        //Array para fila
+        // Array para fila
         return [
             'name' => $regs['agent']->name,
             'email' => $regs['agent']->user->email, 
@@ -49,12 +41,24 @@ class NotificationDiligence {
             'days' => $regs['reg']->opportunity->getMetadata('diligence_days')
         ]; 
     }
-
-//    public function getNotificationAudictor($app, $class)
-//    {
-//        $dili = $app->repo('Diligence\Entities\Diligence')->findOneBy(['registration' => $class->data['registration']]);
-//        $numberRegis = '<a href="'.$app->createUrl('inscricao', $class->data['registration']).'">'.$class->data['registration'].'</a>';
-//        $notification->create($class, sprintf(\MapasCulturais\i::__("Houve uma resposta da prestação de conta: ". $numberRegis)));
-//    }
+    /**
+     * Gera o contexto da mensagem para a notificação
+     * @param $class
+     * @param $type
+     * @return string
+     */
+    public function generateMessage($class, $type) : string
+    {
+        $app = App::i();
+        if($type == 'diligence') {
+            // Mensagem para o proponente
+            $numberRegis = '<a href="'. $app->createUrl('inscricao', $class->data['registration']).'">'.$class->data['registration'].'</a>';
+            return 'Um parecerista abriu uma diligência para você responder na inscrição de número: '.$numberRegis;
+        } else {
+            // Mensagem para o fiscal
+            $numberRegis = '<a href="'.$app->createUrl('inscricao', $class->data['registration']).'">'.$class->data['registration'].'</a>';
+            return "Houve uma resposta para prestação de conta de número: ".$numberRegis;
+        }
+    }
 
 }
