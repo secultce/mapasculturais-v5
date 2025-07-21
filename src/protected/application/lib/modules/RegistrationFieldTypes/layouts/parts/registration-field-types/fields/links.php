b3b9f0ff4fc81c2cd37215518fe057b15f03e7d7
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2025-07-21 15:00:21 -0300

    feat: limite no tamanho da requisição (inscrição)

--- src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/links.php
+++ src/protected/application/lib/modules/RegistrationFieldTypes/layouts/parts/registration-field-types/fields/links.php
@@ -1,15 +1,23 @@
-<?php use MapasCulturais\i; ?>
+<?php
+
+use MapasCulturais\i;
+
+$maxNumberLinks = env('MAX_NUMBER_LINKS') ? (int) env('MAX_NUMBER_LINKS') : 20;
+
+?>
+
 <div ng-repeat="link in entity[fieldName]">
     <label ng-if="::field.config.title" style="display:inline-block">
         <?php i::_e('Título') ?>: <br>
-        <input ng-model="link.title" ng-blur="saveField(field, entity[fieldName])" >
+        <input ng-model="link.title" ng-blur="saveField(field, entity[fieldName])">
     </label>
     <label style="display:inline-block">
         <?php i::_e('URL') ?>: <br>
-        <input ng-model="link.value" ng-blur="saveField(field, entity[fieldName])" placeholder="https://" required >
+        <input ng-model="link.value" ng-blur="saveField(field, entity[fieldName])" placeholder="https://" required>
     </label>
 
     <button ng-click="remove(entity[fieldName], $index); saveField(field, entity[fieldName], 0);" class="btn btn-danger"><?php i::_e('remover') ?></button>
 </div>
+
 {{ (entity[fieldName] = (entity[fieldName] ? entity[fieldName] : []) ) && false ? '' : ''}}
-<button ng-click="entity[fieldName] = entity[fieldName].concat([{}])" class="btn btn-primary"><?php i::_e('adicionar') ?></button>
+<button ng-click="entity[fieldName] = addLinkField(entity[fieldName], <?= $maxNumberLinks ?>)" class="btn btn-primary"><?php i::_e('adicionar') ?></button>
