46d57f484bc23803575bfcf83152442ce25fb6e9
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2025-08-06 15:40:34 -0300

    Ref. plugin PDFReport (imprimir inscrição de todas as fases)

--- src/protected/application/themes/BaseV1/views/pdf/footer-pdf.php
+++ src/protected/application/themes/BaseV1/views/pdf/footer-pdf.php
@@ -5,10 +5,10 @@
             <p style="text-align: center; padding-right: 30px; font-size: 10px;"><span> {PAGENO}{nbpg}</span></p>
             <p style="width: 100%; float: left;font-size: 9px;">
                 <label for="">
-                    <strong>Secretaria da Cultura do Ceara</strong>
+                    <strong>Secretaria da Cultura do Ceará</strong>
                 </label><br>
-                <label for="">Rua Dr. João Moreira, 540 - Centro. CEP: 60.030-000</label><br>
-                <label for="">Fortaleza/CE. Fone: (85) 981152400</label>
+                <label for="">Rua Dr. João Moreira, 540 - Centro. CEP: 60030-000</label><br>
+                <label for="">Fortaleza/CE. Fone: (85) 9 8115-2400</label>
             </p>
         </div>
         <img
