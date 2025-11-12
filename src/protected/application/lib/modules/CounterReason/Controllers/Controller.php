b1f93214dcc525f2fce415c6999f231945d90eb0
Junior-Shyko <franciscoanto@gmail.com>
2025-11-12 13:49:53 -0300

    Verificacao no backend se tem texto da contrarrazao

--- src/protected/application/lib/modules/CounterReason/Controllers/Controller.php
+++ src/protected/application/lib/modules/CounterReason/Controllers/Controller.php
@@ -8,6 +8,7 @@ use MapasCulturais\App;
 use Carbon\Carbon;
 use MapasCulturais\Entities\Agent;
 use MapasCulturais\Entities\Registration;
+use MapasCulturais\Services\SentryService;
 
 
 class Controller extends \MapasCulturais\Controller
@@ -21,16 +22,23 @@ class Controller extends \MapasCulturais\Controller
     public function POST_save()
     {
         $app = App::i();
+
         try {
+
             // 1. Validação básica
             if (!isset($this->data['registration'])) {
                 return $this->json([
                     'error' => 'O valor da inscrição é obrigatório.'
                 ], 400);
             }
-
+            if (empty($this->data['text'])) {
+                return $this->json([
+                    'error' => 'Precisa ter o texto da contrarrazão.'
+                ], 400);
+            }
             // 2. Busca a inscrição
             $registration = $app->repo(Registration::class)->find($this->data['registration']);
+
             if (!$registration) {
                 return $this->json([
                     'error' => 'Inscrição ou Contrarrazão não encontrada.'
@@ -46,17 +54,19 @@ class Controller extends \MapasCulturais\Controller
 
             // 4. Decide: create ou update?
             $existing = CounterReasonRepository::getCounterReason($registration, $app);
+
             if ($existing) {
                 // Atualiza
-                dd($registration);
                 $entity = CounterReasonService::update($registration, $app, $this->data);
+                $app->log->debug('Contrarrazão editada com sucesso.');
             } else {
                 // Cria
                 $entity = CounterReasonService::create($app, $this->data);
+                $app->log->debug('Contrarrazão criada com sucesso.');
             }
             $this->json(['message' => 'success', 'status' => 200, 'entityId' => $entity]);
         }catch (\Exception $e){
-            dump($e);
+            SentryService::captureExceptions($e);
         }
 
     }
