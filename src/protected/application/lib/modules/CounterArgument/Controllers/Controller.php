6f20039f48e1e4719e29c2450fcc416a6d957bc1
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2026-01-28 11:02:42 -0300

    feat: bloqueio da resposta da contrarrazão após sua publicação

--- src/protected/application/lib/modules/CounterArgument/Controllers/Controller.php
+++ src/protected/application/lib/modules/CounterArgument/Controllers/Controller.php
@@ -87,6 +87,7 @@ class Controller extends \MapasCulturais\Controller
         $this->verifyResponsePermission($counterArgument->registration);
         $this->validateResponsePeriod($counterArgument->registration->opportunity);
         $this->verifyResponseOwner($counterArgument->response);
+        $this->verifyPublishedResponse($counterArgument->response);
 
         try {
             $this->counterArgumentService->saveResponse($data);
@@ -173,6 +174,14 @@ class Controller extends \MapasCulturais\Controller
 
         if ($counterArgumentsWithoutResponse) {
             $this->json(['message' => 'Existem contrarrazões sem resposta. Por favor, responda todas as contrarrazões antes de publicar as respostas.'], 403);
+            return;
+        }
+    }
+
+    private function verifyPublishedResponse($response)
+    {
+        if ($response && $response->published) {
+            $this->json(['message' => 'Esta resposta já foi publicada e não pode mais ser editada.'], 403);
             return;
         }
     }
