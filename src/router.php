470a60c785b3dab77da1df57553113ba374c01df
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-02-24 11:07:31 -0300

    fix no router e no docker-compose

--- src/router.php
+++ src/router.php
@@ -7,28 +7,23 @@ if (file_exists($_SERVER['SCRIPT_FILENAME']) && strtolower(substr($_SERVER['SCRI
     header("Pragma: public");
     header("Cache-Control: maxage=" . $expires);
     header('Expires: ' . gmdate('D, d M Y H:i:s', time() + $expires) . ' GMT');
-    $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
-    switch ($ext) {
-        case 'js':
-            $mime = 'text/javascript';
-            break;
-        case 'css':
-            $mime = 'text/css';
-            break;
-        case 'woff2':
-            $mime = 'font/woff2';
-            break;
-        case 'woff':
-            $mime = 'font/woff';
-            break;
-        case 'ttf':
-            $mime = 'font/ttf';
-            break;
-        default:
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            $mime = finfo_file($finfo, $filename);
-            finfo_close($finfo);
+    $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+    $mimeMap = [
+        'js'   => 'text/javascript',
+        'css'  => 'text/css',
+        'woff'  => 'font/woff',
+        'woff2' => 'font/woff2',
+        'ttf'   => 'font/ttf',
+        'eot'   => 'application/vnd.ms-fontobject',
+        'svg'   => 'image/svg+xml',
+    ];
+    if (isset($mimeMap[$ext])) {
+        $mime = $mimeMap[$ext];
+    } else {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $_SERVER['SCRIPT_FILENAME']);
+        finfo_close($finfo);
     }
     header('Content-type: ' . $mime);
 
@@ -40,5 +35,3 @@ if (file_exists($_SERVER['SCRIPT_FILENAME']) && strtolower(substr($_SERVER['SCRI
 }else{
     return false;
 }
-
-
